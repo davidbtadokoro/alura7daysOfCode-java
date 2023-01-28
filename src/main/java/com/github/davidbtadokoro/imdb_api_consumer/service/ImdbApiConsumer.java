11820343648b083a78c78860a24e8a26a31dc472
David Tadokoro <davidbtadokoro@usp.br>
2023-01-28 17:18:48 -0300

    Refactor Day 1 implementation with Extract Method
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/main/java/com/github/davidbtadokoro/imdb_api_consumer/service/ImdbApiConsumer.java
+++ src/main/java/com/github/davidbtadokoro/imdb_api_consumer/service/ImdbApiConsumer.java
@@ -7,6 +7,38 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
 public class ImdbApiConsumer {
+
+  private String apiKey;
+  private HttpClient client;
+  private HttpRequest request;
+  private HttpResponse<String> response;
+
+  public ImdbApiConsumer(String apiKey) {
+    this.apiKey = apiKey;
+    this.client = HttpClient.newBuilder().build();
+  }
+
+  public void sendTop250MoviesRequest() {
+    try {
+      request = HttpRequest
+          .newBuilder()
+          .uri(new URI(String.format("https://imdb-api.com/en/API/Top250Movies/%s", apiKey)))
+          .GET()
+          .build();
+      response = client.send(request, BodyHandlers.ofString());
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public int getResponseStatusCode() {
+    return response.statusCode();
+  }
+
+  public String getResponseBody() {
+    return response.body();
+  }
   
   public static void main(String[] args) {
     if (args.length != 1) {
@@ -15,22 +47,9 @@ public class ImdbApiConsumer {
       System.exit(1);
     }
 
-    String uri = String.format("https://imdb-api.com/en/API/Top250Movies/%s", args[0]);
-
-    try {
-      HttpClient client = HttpClient.newBuilder().build();
-      HttpRequest request = HttpRequest
-          .newBuilder()
-          .uri(new URI(uri))
-          .GET()
-          .build();
-      HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-      System.out.printf("%nSTATUS CODE: %d%n%n", response.statusCode());
-      System.out.printf("BODY: %s", response.body());
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
+    ImdbApiConsumer imdbApiConsumer = new ImdbApiConsumer(args[0]);
+    imdbApiConsumer.sendTop250MoviesRequest();
+    System.out.println(imdbApiConsumer.getResponseStatusCode() + " " + imdbApiConsumer.getResponseBody());
   }
 
 }
