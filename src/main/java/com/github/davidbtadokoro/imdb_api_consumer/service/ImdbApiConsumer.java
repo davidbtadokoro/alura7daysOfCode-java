dd22b1003a00bfebe59cf9086cea21f83b4e1a51
David Tadokoro <davidbtadokoro@usp.br>
2023-01-30 13:04:07 -0300

    Adapts JSON parsing to use Jackson
    This commit changes the JSON parsing implementation to use the Jackson 2
    library. Also, there is only one method for parsing, getMoviesList(),
    that returns the list of the Movie objects. This method represents the
    unmarshalling of the Movie objects from the JSON.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/main/java/com/github/davidbtadokoro/imdb_api_consumer/service/ImdbApiConsumer.java
+++ src/main/java/com/github/davidbtadokoro/imdb_api_consumer/service/ImdbApiConsumer.java
@@ -6,11 +6,17 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.Collectors;
+import java.util.Collections;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+
+import com.github.davidbtadokoro.imdb_api_consumer.model.Movie;
 
 public class ImdbApiConsumer {
 
+  private ObjectMapper mapper = getDefaultObjectMapper();
   private String apiKey;
   private String moviesJson;
 
@@ -24,19 +30,13 @@ public class ImdbApiConsumer {
     ImdbApiConsumer imdbApiConsumer = new ImdbApiConsumer(args[0]);
     imdbApiConsumer.sendTop250MoviesRequest();
 
-    String[] movieArray = imdbApiConsumer.parseMoviesJson();
+    List<Movie> movies = imdbApiConsumer.getMoviesList();
+    movies.forEach(System.out::println);
+  }
 
-    List<String> titles = imdbApiConsumer.parseTitles(movieArray);
-    titles.forEach(System.out::println);
-
-    List<String> urlImages = imdbApiConsumer.parseUrlImages(movieArray);
-    urlImages.forEach(System.out::println);
-
-    List<String> years = imdbApiConsumer.parseYears(movieArray);
-    years.forEach(System.out::println);
-
-    List<String> ratings = imdbApiConsumer.parseRatings(movieArray);
-    ratings.forEach(System.out::println);
+  private static ObjectMapper getDefaultObjectMapper() {
+    return new ObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
   public ImdbApiConsumer(String apiKey) {
@@ -63,35 +63,14 @@ public class ImdbApiConsumer {
     return moviesJson;
   }
 
-  public String[] parseMoviesJson() {
-    String moviesJsonInsideBrackets = moviesJson
-        .substring(moviesJson.indexOf('[') + 1, moviesJson.indexOf(']'));
-    return moviesJsonInsideBrackets
-        .substring(1, moviesJsonInsideBrackets.length() - 1)
-        .split("\\},\\{");
-  }
-  
-  public List<String> parseTitles(String[] moviesArray) {
-    return parseAttribute(moviesArray, 2);
-  }
-  
-  public List<String> parseYears(String[] moviesArray) {
-    return parseAttribute(moviesArray, 4);
-  }
-  
-  public List<String> parseUrlImages(String[] moviesArray) {
-    return parseAttribute(moviesArray, 5);
-  }
-  
-  public List<String> parseRatings(String[] moviesArray) {
-    return parseAttribute(moviesArray, 7);
-  }
-
-  private List<String> parseAttribute(String[] movieArray, int position) {
-    return Stream.of(movieArray)
-        .map(e -> e.split("\",\"")[position])
-        .map(e -> e.split(":\"")[1])
-        .collect(Collectors.toList());
+  public List<Movie> getMoviesList() {
+    try {
+      String moviesJsonArray = mapper.readTree(moviesJson).get("items").toString();
+      return mapper.readValue(moviesJsonArray, new TypeReference<List<Movie>>(){});
+    } catch (Exception e) {
+      e.printStackTrace();
+      return Collections.emptyList();
+    }
   }
 
 }
